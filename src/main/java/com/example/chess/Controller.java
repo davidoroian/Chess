0c0ce9d3a6553558a2aeac086d4bd09a71ec6ece
VladVeliciu <vlad.veliciu@yahoo.com>
2022-07-27 19:48:03 +0300

    Move the pieces in background (console) while getting the indices from the table by mouse

--- src/main/java/com/example/chess/Controller.java
+++ src/main/java/com/example/chess/Controller.java
@@ -13,6 +13,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -22,13 +23,15 @@ public class Controller implements Initializable {
     private ImageView myPawnWhite;
 
     private Game game;
-    private int indexC, indexR;
+    private int x, y;
+    private int count=1;//the first click when opening the app is not counted
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         for (int i=0;i<8;i++)
             for (int j=0;j<8;j++) {
                 AnchorPane anchorPane=new AnchorPane();
+                //set the chessboard design
                 if (i%2==0) {
                     if (j % 2 != 0)
                         anchorPane.setBackground(new Background(new BackgroundFill(Color.color(0.411, 0.411, 0.411), null, null)));
@@ -48,15 +51,26 @@ public class Controller implements Initializable {
                 ChessBoard.getChildren().add(anchorPane);
             }
         game=new Game();
-
     }
 
     @FXML
-    public void mouseClicked(MouseEvent e){
+    public void mouseClicked(MouseEvent e){//get the indices where the mouse is clicked
         Node node= (Node) e.getTarget();
-        indexC= GridPane.getColumnIndex(node);
-        indexR= GridPane.getRowIndex(node);
-        game.gameMethod(indexR,indexC,0,0);
+        y= GridPane.getColumnIndex(node);
+        x= GridPane.getRowIndex(node);
+        count++;//count to know that the piece would go from a pair of indices to another
+    }
+
+    public void getToXY(MouseEvent e){//when releasing the mouse
+        int toX, toY;
+        Node node= (Node) e.getTarget();
+        toY= GridPane.getColumnIndex(node);
+        toX= GridPane.getRowIndex(node);
+        if (count==2) {//call the method from game only when we have the two pair of indices
+            game.playGameMethod(x, y, toX, toY);
+            count=0;//reset count to 0
+        }
+
     }
 
 }
